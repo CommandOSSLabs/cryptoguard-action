9659f83af0d5cd52e2d70dc8c2487d21d57e4800
jayden-dang <jayden@jaydendang.com>
2025-10-07 09:12:57 +0700

    feat: refactor action attestation workflow

--- dist/7.index.js
+++ dist/7.index.js
@@ -3,7 +3,7 @@ exports.id = 7;
 exports.ids = [7];
 exports.modules = {
 
-/***/ 28007:
+/***/ 8007:
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
